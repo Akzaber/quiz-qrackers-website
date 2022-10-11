a2d86e4b81bc99ddaece200e1e471eb4d487b3c6
Akzaber <zaberkhan611@gmail.com>
2022-10-11 20:58:34 +0600

    Answer show Done

--- src/components/QuizesQ/QuizesQ.js
+++ src/components/QuizesQ/QuizesQ.js
@@ -7,27 +7,42 @@ const QuizesQ = ({quizsQ}) => {
 
   const {id, correctAnswer, options, question} = quizsQ;
 
-  const success = () => toast.success(' Your Answer is Correct!', {
-    position: "top-center",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-    });
-
-    const error = () => toast.error(' You Answer is Incorrect!', {
-      position: "bottom-center",
+    const success = () => toast.success(' Your Answer is Correct!', {
+      position: "top-center",
       autoClose: 2000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
-      draggable: true,
+      draggable: false,
       progress: undefined,
       theme: "colored",
+    });
+
+    const error = () => toast.error(' You Answer is Incorrect!', {
+        position: "bottom-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
       });
+
+      const showAnswer = () => toast.info( `${correctAnswer}`, {
+        position: "bottom-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+
+  const handleShow = () => {
+    showAnswer();
+  }
 
 
   const handleCorrect = (option) => {
@@ -44,14 +59,15 @@ const QuizesQ = ({quizsQ}) => {
     <div className='bg-blue-900 rounded-md w-9/12 mx-auto mb-4 p-2 text-white'>
       <div className='flex justify-between items-center'>
         <p><strong>Question: {question}</strong></p>
-        <button><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></button>
+        <button onClick={handleShow}><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></button>
       </div>
      <div className='grid grid-cols-2 mx-auto'>
         {
           options.map((option, index) => 
               <div key={index} className='border border-white m-2 rounded-md text-left p-5'>
               <button onClick={() => handleCorrect(option)}><input type="radio" name={id} value={option} id={option} />
-              <label htmlFor={option}>{option}</label></button>
+              </button>
+              <label htmlFor={option}>{option}</label>
               <ToastContainer>
               
               </ToastContainer>
